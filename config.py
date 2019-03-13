4733f9bed83bac7d21e806b88935ed87f3bfa51a
Juho Saarinen <juho.saarinen@gmail.com>
2019-03-13 12:36:46 +0200

    Added CSS
    Using sqliteDB

--- config.py
+++ config.py
@@ -6,9 +6,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'
 
 # The SQLAlchemy connection string.
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/app.db'
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:rf-server@/rf_server?unix_socket=/cloudsql/rf-server-dev:europe-west1:rf-server-dev-mysql'
 #SQLALCHEMY_DATABASE_URI = 'mysql://myapp@localhost/myapp'
-#SQLALCHEMY_DATABASE_URI = 'postgresql://root:password@localhost/myapp'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://postrgres:postgres@/rf-server'
 
 # Flask-WTF flag for CSRF
 CSRF_ENABLED = True
